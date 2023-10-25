dd5c400de843ea47197fcd1b0779498202c468a3
KMagdi <46417302+kmagdi@users.noreply.github.com>
2023-10-25 13:31:48 +0200

    first commit

---
+++ routing.js
@@ -0,0 +1,39 @@
+// Objektum, amely a hash értékekhez tartozó útvonalakat tartalmazza
+const routes = {
+  '': 'home.html',
+  'about': 'about.html',
+  'contact': 'contact.html'
+};
+
+
+
+//első látogatáskor, az oldal betöltődésekor:
+navigate();
+
+// A hash változását figyeljük, és frissítjük a tartalmat
+window.addEventListener("hashchange", () => {
+  console.log(window.location.hash);
+  navigate();
+});
+
+
+// A megfelelő oldalt jelenítjük meg a hash alapján
+function navigate() {
+  const domObj = document.getElementById("content");
+  // elmentjük a jelenlegi URL hash értékét.
+  const hash = window.location.hash;
+  const route = hash.substring(2); // Az '#/' előtag eltávolítása
+  const page = routes[route] || '404.html';
+  loadPage(page, domObj);
+}
+
+
+async function loadPage(url, domObj) {
+  try {
+   const response = await fetch(url);
+   const html = await response.text();
+   domObj.innerHTML = html;
+  } catch (error) {
+    console.error(`Hiba történt: ${error}`);
+  }
+}
